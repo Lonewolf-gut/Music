1702cbcd89dc5bb43b60210274738a36fbbade14
Eric Krampah <ekrampah253@gmail.com>
2025-04-09 15:52:17 +0100

    3 components created

--- music-player-app/src/components/SearchBar.jsx
+++ music-player-app/src/components/SearchBar.jsx
@@ -1,24 +1,24 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-// const SearchBar = ({ onSearch }) => {
-//   const [query, setQuery] = useState('');
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
 
-//   const handleSearch = (event) => {
-//     event.preventDefault();
-//     onSearch(query);
-//   };
+  const handleSearch = (event) => {
+    event.preventDefault();
+    onSearch(query);
+  };
 
-//   return (
-//     <form onSubmit={handleSearch} className="w-full p-4">
-//       <input
-//         type="text"
-//         className="w-full p-2 border rounded-lg"
-//         placeholder="Search for songs, artists, or albums"
-//         value={query}
-//         onChange={(e) => setQuery(e.target.value)}
-//       />
-//     </form>
-//   );
-// };
+  return (
+    <form onSubmit={handleSearch} className="w-full p-4">
+      <input
+        type="text"
+        className="w-full p-2 border rounded-lg"
+        placeholder="Search for songs, artists, or albums"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+};
 
-// export default SearchBar;
+export default SearchBar;
