ff15b9bb0f1a8666c4d349256adf3b3b7eda9d3d
Eric Krampah <ekrampah253@gmail.com>
2025-04-09 15:58:31 +0100

    Error catching

--- music-player-app/src/Errors/ErrorHandlg.js
+++ music-player-app/src/Errors/ErrorHandlg.js
@@ -1,14 +1,16 @@
-// const searchTracks = async (query) => {
-//     try {
-//       const response = await fetch(`https://api.deezer.com/search?q=${query}`);
-//       const data = await response.json();
-//       if (!data.data || data.data.length === 0) {
-//         alert("No results found for this query.");
-//       }
-//       return data;
-//     } catch (error) {
-//       console.error("Error fetching tracks", error);
-//       alert("An error occurred while fetching the tracks.");
-//     }
-//   };
+
+
+const searchTracks = async (query) => {
+    try {
+      const response = await fetch(`https://api.deezer.com/search?q=${query}`);
+      const data = await response.json();
+      if (!data.data || data.data.length === 0) {
+        alert("No results found for this query.");
+      }
+      return data;
+    } catch (error) {
+      console.error("Error fetching tracks", error);
+      alert("An error occurred while fetching the tracks.");
+    }
+  };
   
